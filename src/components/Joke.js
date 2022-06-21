4129699fa360debbb4b4f5b0efb5349560a86295
Tristan <tsilvas0113@gmail.com>
2022-06-21 11:17:14 -0500

    Setup Project

---
+++ src/components/Joke.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Joke () {
+    return (
+        <div>
+            <h2>Setup</h2>
+            <h1>Punchline</h1>
+        </div>
+    )
+}
+
+export default Joke
